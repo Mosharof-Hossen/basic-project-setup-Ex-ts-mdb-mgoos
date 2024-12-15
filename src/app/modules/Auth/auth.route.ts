abcaf9b59285877f93f5078c2e7fcd1f1c3cf0b0
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-12-15 18:21:06 +0600

    auth interface, route, controller done

---
+++ src/app/modules/Auth/auth.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import dataValidator from "../../middlwares/dataValidator";
+import { AuthValidation } from "./auth.validation";
+import { AuthController } from "./auth.controller";
+
+const router = Router();
+
+router.post(
+    '/login',
+    dataValidator(AuthValidation.loginValidationSchema),
+    AuthController.loginUser
+)
---
+++ src/app/modules/Auth/auth.services.ts
@@ -0,0 +1,10 @@
+import { TLoginUser } from "./auth.interface";
+
+const loginUser = async (payload: TLoginUser) => {
+
+}
+
+
+export const AuthServices = {
+    loginUser
+}
